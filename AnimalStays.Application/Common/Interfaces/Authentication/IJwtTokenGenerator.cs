c552679a02ead0529c7b4f50c8e854fadc6641fe
rwaldron <outsideinprofits@gmail.com>
2023-08-29 16:46:50 +1000

    Refactor: Authentication now uses User Repository

--- AnimalStays.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
+++ AnimalStays.Application/Common/Interfaces/Authentication/IJwtTokenGenerator.cs
@@ -1,10 +1,8 @@
+using AnimalStays.Domain.Entities;
+
 namespace AnimalStays.Application.Common.Interfaces.Authentication;
 
 public interface IJwtTokenGenerator
 {
-  string GenerateToken(
-    Guid userId,
-    string firstName,
-    string lastName
-    );
+  string GenerateToken(User user);
 }
--- AnimalStays.Infrastructure/DependencyInjection.cs
+++ AnimalStays.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,8 @@
 using AnimalStays.Application.Common.Interfaces.Authentication;
+using AnimalStays.Application.Common.Interfaces.Persistence;
 using AnimalStays.Application.Common.Interfaces.Services;
 using AnimalStays.Infrastructure.Authentication;
+using AnimalStays.Infrastructure.Persistence;
 using AnimalStays.Infrastructure.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +18,7 @@ public static class DependencyInjection
     services.Configure<JwtSettings>(configuration.GetSection(JwtSettings.SectionName));
     services.AddSingleton<IJwtTokenGenerator, JwtTokenGenerator>();
     services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+    services.AddScoped<IUserRepository, UserRepository>();
     return services;
   }
 }
